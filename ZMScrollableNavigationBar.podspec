895f97e8764afa8e8b08a6f0d2db9f36abdc0f4b
Zouhair Mahieddine <zouhair.mahieddine@gmail.com>
2015-03-23 21:02:25 -0700

    Updated podspec + fixed some bugs

--- ZMScrollableNavigationBar.podspec
+++ ZMScrollableNavigationBar.podspec
@@ -1,7 +1,7 @@
 Pod::Spec.new do |s|
 
   s.name         = "ZMScrollableNavigationBar"
-  s.version      = "0.2.0"
+  s.version      = "0.3.0"
   s.summary      = "A UINavigationController subclass that allows the UINavigationBar to scroll with the content (iOS 8 only)."
 
   s.homepage     = "https://github.com/Zedenem/ZMScrollableNavigationBar"
@@ -10,9 +10,9 @@ Pod::Spec.new do |s|
 
   s.author       = { "Zouhair Mahieddine" => "zouhair.mahieddine@gmail.com" }
 
-  s.platform     = :ios, '8.0'
+  s.platform     = :ios, '7.0'
 
-  s.source       = { :git => "https://github.com/Zedenem/ZMScrollableNavigationBar.git", :tag => "0.2.0" }
+  s.source       = { :git => "https://github.com/Zedenem/ZMScrollableNavigationBar.git", :tag => "0.3.0" }
 
   s.source_files  = 'ZMScrollableNavigationBar/*.{swift}'
   
--- ZMScrollableNavigationBar/ZMScrollableNavigationController.swift
+++ ZMScrollableNavigationBar/ZMScrollableNavigationController.swift
@@ -14,12 +14,33 @@ let minTopOffset: CGFloat = -44.0
 extension UINavigationController {
   var topOffset: CGFloat {
     set(newTopOffset) {
-      var topOffset = min(max(newTopOffset, minTopOffset), maxTopOffset)
+      if self.navigationBarHidden {
+        return
+      }
+      
+      var topOffset: CGFloat = newTopOffset
+      
+      switch UIDevice.currentDevice().systemVersion.compare("8.0.0", options: NSStringCompareOptions.NumericSearch) {
+      case .OrderedSame, .OrderedDescending:break
+      case .OrderedAscending:
+        // iOS < 8.0.0 not supported at the time
+        topOffset = 0.0
+        return
+      }
+      
+      // Adjust to the presence of a UIStatusBar
+      if !UIApplication.sharedApplication().statusBarHidden {
+        topOffset -= CGRectGetHeight(UIApplication.sharedApplication().statusBarFrame)
+      }
+      
+      // Adjust so that the view doesn't scroll below the UINavigationBar before it starts hiding
+      topOffset -= CGRectGetHeight(self.navigationBar.frame)
+      
+      topOffset = min(max(topOffset, minTopOffset), maxTopOffset)
       if topOffset != self.topOffset {
         var frame = self.view.frame
         frame.origin.y = CGFloat(topOffset)
         frame.size.height = self.view.superview!.frame.size.height + CGFloat(-topOffset)
-        println("New Frame: " + NSStringFromCGRect(self.view.frame))
         self.view.frame = frame
       }
     }
